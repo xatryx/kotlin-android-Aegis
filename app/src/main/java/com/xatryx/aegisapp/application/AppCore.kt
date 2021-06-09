d0ed2aea91f69ce97e0f7e8ef73c76d294f3494b
Ronan Harris <yuunme@protonmail.com>
2021-06-09 17:42:42 +0700

    initialized DiscordReposiroty on AppCore

--- app/src/main/java/com/xatryx/aegisapp/application/AppCore.kt
+++ app/src/main/java/com/xatryx/aegisapp/application/AppCore.kt
@@ -1,17 +1,16 @@
 package com.xatryx.aegisapp.application
 
 import android.app.Application
+import com.xatryx.aegisapp.DiscordRepository
 import com.xatryx.aegisapp.util.OFuelInstance
-import org.kodein.di.DI
-import org.kodein.di.DIAware
+import org.kodein.di.*
 import org.kodein.di.android.x.androidXModule
-import org.kodein.di.bind
-import org.kodein.di.singleton
 
 class AppCore : Application(), DIAware {
     override val di: DI by DI.lazy {
         import(androidXModule(this@AppCore))
 
         bind { singleton { OFuelInstance.init() } }
+        bind { singleton { DiscordRepository(instance()) } }
     }
 }
